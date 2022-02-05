d0ca3a0f195368178b8f287bdc01159b1b52178b
Luong Hieu Thi <luonghieuthi@gmail.com>
2022-02-05 21:48:58 +0900

    Update plugin manifest

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { ContentScriptType } from "api/types";
 
 joplin.plugins.register({
 	onStart: async function() {
-		console.info('Hello world. Test plugin started!');
 		await joplin.contentScripts.register(
 			ContentScriptType.MarkdownItPlugin,
 			'lifeMdRule',
