bfe4f8d23492e01cc9e9fe8df89a8d287f0e5e8a
Luong Hieu Thi <luonghieuthi@gmail.com>
2022-12-23 12:23:19 +0900

    Simplify Custom Mode

--- src/lifeMdCtrl.js
+++ src/lifeMdCtrl.js
@@ -1,10 +1,11 @@
 function plugin(CodeMirror) {
-  CodeMirror.defineMode("life", function(conf) {
-    var lifeMode = {
-      token: function(stream, state) { while (stream.next() != null ) {}; return null; }
-    }
-    return CodeMirror.overlayMode(CodeMirror.getMode(conf, "text/x-yaml"), lifeMode);
-  });
+  // CodeMirror.defineMode("life", function(conf) {
+  //   var lifeMode = {
+  //     token: function(stream, state) { while (stream.next() != null ) {}; return null; }
+  //   }
+  //   return CodeMirror.overlayMode(CodeMirror.getMode(conf, "text/x-yaml"), lifeMode);
+  // });
+  CodeMirror.defineMode("life", function(conf) { return CodeMirror.getMode(conf, "text/x-yaml"); });
 }
 module.exports = {
   default: function(_context) {
